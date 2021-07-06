76d4567a0fb1430821c5743f086ade5e8eea16da
SOLANGE\solange <solangecarlaraqui@gmail.com>
2021-07-05 19:31:46 -0500

    se avanzó con el modelado 16:49

---
+++ ECOMERCE/ECOMERCE/Models/BookDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECOMERCE.Models
+{
+    public partial class BookDBContext : DbContext
+    {
+        public BookDBContext()
+        {
+
+        }
+        public BookDBContext(DbContextOptions<BookDBContext> options) : base(options)
+        {
+
+
+        }
+
+        public virtual DbSet<Regalos> Regalos
+        {
+            get; set;
+        }
+    }
+}
