94ec027e1db4c4561684d648f7034c72558565b2
christian <christian1998inf@gmail.com>
2021-07-23 21:54:44 -0400

    arreglos de las plantillas y url

--- Ecomerce/gestionProductos/models.py
+++ Ecomerce/gestionProductos/models.py
@@ -2,25 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class Productos(models.Model):
-    id=models.IntegerField()
-    nombre=models.CharField(max_length=20)
-    seccion
-
-class Usuario(models.Model):
-
-
-class Seccion(models.Model):
-
-
-class Pedidos(models.Model):
-
-
-class Stock(models.Model):
-
-
-class Fecha(models.Model):
-
-
-
-    
