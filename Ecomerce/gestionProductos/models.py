216ebba1dfd2a0a48a3647a4af9d3e2bf67d8025
SOLANGE\solange <solangecarlaraqui@gmail.com>
2021-08-03 19:45:33 -0500

    migracion de base de datos a sql

--- Ecomerce/gestionProductos/models.py
+++ Ecomerce/gestionProductos/models.py
@@ -1,26 +1,35 @@
 from django.db import models
 
 # Create your models here.
-
-class Productos(models.Model):
-    id=models.IntegerField()
-    nombre=models.CharField(max_length=20)
-    seccion
-
-class Usuario(models.Model):
+#class Usuario(models.Model):
 
 
-class Seccion(models.Model):
+#class Seccion(models.Model):
+
+
+#class Pedidos(models.Model):
+
+
+#class Stock(models.Model):
+
+
+#class Fecha(models.Model):
+class Producto(models.Model):
+    Id_Producto=models.IntegerField()
+    Nombre=models.CharField(max_length=30)
+    Stock=models.IntegerField()
+    Precio=models.IntegerField()
 
 
 class Pedidos(models.Model):
-
-
-class Stock(models.Model):
-
-
-class Fecha(models.Model):
-
+    Id_Pedido=models.IntegerField()
+    Nombre=models.CharField(max_length=30)
+    Apellido=models.CharField(max_length=30)
+    Correo=models.EmailField()
+    Telefono=models.IntegerField
+    Fecha=models.DateField()
+    Entregado=models.BooleanField()
+    Comentario=models.CharField(max_length=300)
 
 
     
